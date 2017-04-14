325fa370ae7204274edd225d7e2e2cf4de61a796
Sophia Veksler <sophia.veksler@wustl.edu>
2017-04-14 13:58:49 -0500

    lol

--- pieChart/pieChart.js
+++ pieChart/pieChart.js
@@ -2,7 +2,7 @@ function pieChart(filename) {
 var margin = {top: 20, bottom: 20, left: 60, right: 60};
 var width = 750 - margin.left - margin.right;
 var height = 750 - margin.top - margin.bottom;
-var radius = Math.min(width, height) / 2,
+var radius = Math.min(width, height) / 2;
 
 
 d3.csv(filename, function(data) {
