37216671b1397c9c96d3b334868b3cdf4e8d6a4a
evans-mara <evans.mara@wustl.edu>
2017-04-24 21:42:27 -0500

    dftyh

--- highLow/highLow.js
+++ highLow/highLow.js
@@ -2,8 +2,10 @@ function highLow(filename) {
 
 
 var category = document.getElementById("category").value;
+var unit = document.getElementById("unit").value;
 
-console.log(category);
+// console.log(category);
+// console.log(unit);
 
 var margin = {top: 20, bottom: 20, left: 60, right: 60};
 var width = 1300 - margin.left - margin.right;
@@ -22,11 +24,7 @@ var tooltip = d3.select("body").append("div")
 
 d3.csv(filename, function(data) {
 		var allKeys = d3.keys(data[0]);
-		console.log(allKeys);
 		var keys = allKeys;
-		console.log(keys);
-
-		//console.log(data);
 
 		var restaurants = ["Arby's", "Burger King", "Carl's Jr", "Chick-fil-a", "In-N-Out", "Jack in the Box", "KFC", "McDonald's", "Popeye's", "Sonic", "Taco Bell", "Wendy's", "Whataburger"];
 		
@@ -37,35 +35,26 @@ d3.csv(filename, function(data) {
 
 		for (var i = 0; i < restaurants.length; i++) {
 			barMaxVals[restaurants[i]] = 0;
-			barMinVals[restaurants[i]] = 999;
+			barMinVals[restaurants[i]] = 9999;
 			barMaxName[restaurants[i]] = "";
-			barMinName[restaurants[i]] = "";
-
-			
+			barMinName[restaurants[i]] = "";	
 		}
-
+var i = 0;
 		for (object in data) {
-			//console.log(object);
-			//console.log(data[object]["Calories"]);
-			//console.log(data[object]["Restaurant"]);
-
-			// console.log(data[object]["Category"] == category);
+			
 			if (data[object]["Category"] == category) {			
-				if (barMaxVals[data[object]["Restaurant"]] < data[object]["Calories"]) {
-					barMaxVals[data[object]["Restaurant"]] = data[object]["Calories"];
+				i++;
+				if (parseInt(barMaxVals[data[object]["Restaurant"]]) < parseInt(data[object][unit])) {
+					barMaxVals[data[object]["Restaurant"]] = data[object][unit];
 					barMaxName[data[object]["Restaurant"]] = data[object]["Item"];
 				}
-				if (barMinVals[data[object]["Restaurant"]] > data[object]["Calories"]) {
-					barMinVals[data[object]["Restaurant"]] = data[object]["Calories"];
+				if (parseInt(barMinVals[data[object]["Restaurant"]]) > parseInt(data[object][unit])) {
+					barMinVals[data[object]["Restaurant"]] = data[object][unit];
 					barMinName[data[object]["Restaurant"]] = data[object]["Item"];
 				}
 			}
 		}
-
-		 // console.log(barMaxVals);
-		 // console.log(barMinVals);
-		// console.log(barMaxName);
-		// console.log(barMinName);
+		//console.log(i);
 
 		var xScale = d3.scaleLinear()
 						.range([0, width])
@@ -91,17 +80,35 @@ d3.csv(filename, function(data) {
 
 
 		for(obj in barMaxVals){
+			console.log(barMaxName[obj]);
+			console.log(barMaxVals[obj]);
+			console.log(barMinName[obj]);
+			console.log(barMinVals[obj]);
+			if(parseInt(barMinVals[obj]) == parseInt(9999)){
+				console.log(barMinName[obj]);
+				barMinVals[obj] = 1.5;
+			}
+			if(parseInt(barMaxVals[obj]) == parseInt(0)){
+				console.log(barMaxName[obj]);
+				barMaxVals[obj] = 1.5;
+			}
 			array.push({
 				key: obj,
 				num: barMaxVals[obj],
 				itemName: barMaxName[obj]
 			});
+			if(parseInt(barMinVals[obj]) == 0){
+				barMinVals[obj] = 1.5;
+			}
 			array.push({
 				key: obj,
 				num: barMinVals[obj],
 				itemName: barMinName[obj]
 			});
 		}
+		// console.log(barMaxVals);
+		// console.log(barMinVals);
+		// console.log(array);
 
 		var colorScale = d3.scaleOrdinal(d3.schemeCategory10);
 		//console.log(maxY);
@@ -124,21 +131,29 @@ d3.csv(filename, function(data) {
 			  	return xScale(array.indexOf(d));
 			  })
 			  .attr("y", function(d) {
-			  	//console.log(d.num);
+			  	console.log(d.itemName);
+			  	console.log(yScale(d.num));
 			  	return yScale(d.num);
 			  })
 			  .attr("height", function(d) {
-			  	return d.num;
+			  	console.log(d.num);
+			  	return height+d.num;
 			  })
 			  .attr("width", 20)
 			  .on("mouseover", function(d) {
                         var ev = 0;                        
-                        if(d!=null){
+                        if(d!=null && d.num!=1.5){
 	                       tooltip.html("<strong style=\"color:red\">" + d.key + "<br>" + "</strong>" + "<strong style=\"color:rgb(91,121,145)\">" + d.itemName + "</strong>" + "<p style=\"color:black\">" + d.num)  
 	                        .style("left", (d3.event.pageX) + "px")     
 	                        .style("top", (d3.event.pageY - 28) + "px")
 	                        .style("opacity", 1);  
-                      }
+                      	}
+                      	if(d!=null && d.num==1.5){
+	                       tooltip.html("<strong style=\"color:red\">" + d.key + "<br>" + "</strong>" + "<strong style=\"color:rgb(91,121,145)\">" + d.itemName + "</strong>" + "<p style=\"color:black\">" + "N/A")  
+	                        .style("left", (d3.event.pageX) + "px")     
+	                        .style("top", (d3.event.pageY - 28) + "px")
+	                        .style("opacity", 1);  
+                      	}
                     })                  
               .on("mouseout", function(d) {           
                         tooltip.style("opacity", 0);    
